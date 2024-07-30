356154223a3c8bc0f620f8d397844d1ae53bad5f
Aaradhya Poudyal <aaradhyapoudyal@Aaradhyas-MacBook-Air.local>
2024-07-29 23:14:21 -0700

    Broke Header and UserInput into individual component files

---
+++ src/Components/UserInput.jsx
@@ -0,0 +1,14 @@
+
+export default function UserInput(){
+    return(
+        <>
+            <label id="input-group">Initial Investment</label>
+            <input id="user-input" type="number"/>    
+        </>
+            
+            
+            
+            
+       
+    )
+}
