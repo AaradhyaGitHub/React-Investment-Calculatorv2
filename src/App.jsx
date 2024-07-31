af575675442a8f95cd48fbde68cb0c16d3de8c89
AaradhyaGitHub <56614526+AaradhyaGitHub@users.noreply.github.com>
2024-07-31 21:56:46 +0000

    Final Edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import Header from "./Components/Header.jsx"
-import UserInput from "./Components/UserInput.jsx"
+import { useState } from "react";
+
+import Header from "./Components/Header.jsx";
+import UserInput from "./Components/UserInput.jsx";
+import Results from "./Components/Results.jsx";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 130,
+  });
+
+ 
+
+  function handleChange(newValue, inputIdentifier) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+        //adding a plus here prevents jS to concatenate the strings and treats the input as numbers which is what we want
+      };
+    });
+  }
+
+  
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      <Results input={userInput} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/UserInput.jsx
+++ src/Components/UserInput.jsx
@@ -1,22 +1,4 @@
-import { useState } from "react";
-
-export default function UserInput() {
-  const [userInput, setUserInput] = useState({
-    initialInvestment: "",
-    annualInvestment: "",
-    expectedReturn: "",
-    duration: "",
-  });
-
-  function handleChange(newValue, inputIdentifier) {
-    setUserInput((prevUserInput) => {
-      return {
-        ...prevUserInput,
-        [inputIdentifier]: newValue,
-      };
-    });
-  }
-
+export default function UserInput({ onChange, userInput }) {
   return (
     <section id="user-input">
       <div className="input-group">
@@ -25,10 +7,11 @@ export default function UserInput() {
             Initial Investment
             <input
               type="number"
+              //even though we specify number here, when doing event.target, we always get string. 
               required
               value={userInput.initialInvestment}
               onChange={(event) =>
-                handleChange("initialInvestment", event.target.value)
+                onChange("initialInvestment", event.target.value)
               }
             />
           </label>
@@ -41,7 +24,7 @@ export default function UserInput() {
               required
               value={userInput.annualInvestment}
               onChange={(event) =>
-                handleChange("annualInvestment", event.target.value)
+                onChange("annualInvestment", event.target.value)
               }
             />
           </label>
@@ -56,7 +39,7 @@ export default function UserInput() {
               required
               value={userInput.expectedReturn}
               onChange={(event) =>
-                handleChange("expectedReturn", event.target.value)
+                onChange("expectedReturn", event.target.value)
               }
             />
           </label>
@@ -68,7 +51,7 @@ export default function UserInput() {
               type="number"
               required
               value={userInput.duration}
-              onChange={(event) => handleChange("duration", event.target.value)}
+              onChange={(event) => onChange("duration", event.target.value)}
             />
           </label>
         </p>
